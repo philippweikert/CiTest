2ee516056639477cd75b4c48870dfc4a23086c7f
philipp.weikert <philippweikert3@gmail.com>
2022-02-15 11:24:45 +0100

    dummes Philipp

--- src/main/java/com/example/citest/CiTestApplication.java
+++ src/main/java/com/example/citest/CiTestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CiTestApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] ) {
         SpringApplication.run(CiTestApplication.class, args);
     }
 
