7f5b16f5c27f9be1b0d45b5fd7bc909da9326a46
philipp.weikert <philippweikert3@gmail.com>
2022-02-15 11:27:55 +0100

    fix von kaputt

--- src/main/java/com/example/citest/CiTestApplication.java
+++ src/main/java/com/example/citest/CiTestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CiTestApplication {
 
-    public static void main(String[] ) {
+    public static void main(String[] args) {
         SpringApplication.run(CiTestApplication.class, args);
     }
 
